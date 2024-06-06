93b07339c0e2606995df3e7bdc21b154bbe878d6
Andrea Barberio <insomniac@slackware.it>
2024-06-06 14:00:40 +0200

    oncall plugin: can search schedules, and better formatting
    Signed-off-by: Andrea Barberio <insomniac@slackware.it>

--- plugins/oncall/oncall.go
+++ plugins/oncall/oncall.go
@@ -77,37 +77,60 @@ func (g *Oncall) get(scheduleID string) ([]pagerduty.OnCall, error) {
 
 // HandleCmd is called when a .wea/.weather command is invoked.
 func (g *Oncall) HandleCmd(client *socketmode.Client, ev *slackevents.MessageEvent, arg string) error {
-	scheduleID := arg
-	if scheduleID == "" {
-		scheduleID = g.Config.DefaultScheduleID
-	}
-	if scheduleID == "" {
-		return fmt.Errorf("Invalid empty schedule ID")
-	}
-	log.Printf("Getting oncalls for schedule ID %q", scheduleID)
-	oncalls, err := g.get(scheduleID)
-	if err != nil {
-		return err
-	}
-	oncallByRotation := make(map[string][]pagerduty.OnCall)
-	for _, oncall := range oncalls {
-		oncallByRotation[oncall.Schedule.Summary] = append(oncallByRotation[oncall.Schedule.Summary], oncall)
-	}
-	for sched, oncalls := range oncallByRotation {
-		msg := sched
-		idx := 0
-		for _, oncall := range oncalls {
-			switch idx {
-			case 0:
-				msg += fmt.Sprintf(": <%s|%s>.", oncall.User.HTMLURL, oncall.User.Summary)
-			case 1:
-				msg += fmt.Sprintf(" Next: <%s|%s>", oncall.User.HTMLURL, oncall.User.Summary)
-			default:
-				msg += fmt.Sprintf(", <%s|%s>", oncall.User.HTMLURL, oncall.User.Summary)
-			}
-			idx++
+	var scheduleIDs []string
+	if arg == "" {
+		scheduleIDs = []string{g.Config.DefaultScheduleID}
+	} else {
+		// search schedules by name
+		pdclient := pagerduty.NewClient(credentials.PagerDutyAPIKey)
+		ctx := context.Background()
+		opts := pagerduty.ListSchedulesOptions{
+			Query: arg,
 		}
-		actions.Say(client, ev.Channel, msg)
+		resp, err := pdclient.ListSchedulesWithContext(ctx, opts)
+		if err != nil {
+			return fmt.Errorf("schedule search failed: %w", err)
+		}
+		for _, sc := range resp.Schedules {
+			scheduleIDs = append(scheduleIDs, sc.ID)
+		}
+	}
+	if scheduleIDs == nil {
+		return fmt.Errorf("invalid empty schedule ID")
+	}
+	log.Printf("Getting oncalls for schedule IDs %v", scheduleIDs)
+	for _, scheduleID := range scheduleIDs {
+		oncalls, err := g.get(scheduleID)
+		if err != nil {
+			return err
+		}
+		oncallByRotation := make(map[string][]pagerduty.OnCall)
+		for _, oncall := range oncalls {
+			oncallByRotation[oncall.Schedule.Summary] = append(oncallByRotation[oncall.Schedule.Summary], oncall)
+		}
+		for sched, oncalls := range oncallByRotation {
+			var msg string
+			if len(oncalls) > 0 {
+				// assume that the schedule URL is the same for all the other
+				// items, since they were grouped together by schedule name.
+				msg += fmt.Sprintf("*<%s|%s>*", oncalls[0].Schedule.HTMLURL, sched)
+			} else {
+				msg += "*" + sched + "*"
+			}
+			idx := 0
+			for _, oncall := range oncalls {
+				switch idx {
+				case 0:
+					msg += fmt.Sprintf(": Current oncall: <%s|%s>.", oncall.User.HTMLURL, oncall.User.Summary)
+				case 1:
+					msg += fmt.Sprintf(" Next: <%s|%s>", oncall.User.HTMLURL, oncall.User.Summary)
+				default:
+					msg += fmt.Sprintf(", <%s|%s>", oncall.User.HTMLURL, oncall.User.Summary)
+				}
+				idx++
+			}
+			actions.Say(client, ev.Channel, msg)
+		}
 	}
 	return nil
 }
